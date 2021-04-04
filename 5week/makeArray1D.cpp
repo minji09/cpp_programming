72fa8af1c301593ecda1f10331d94bc326dce800
minji09 <eming98@naver.com>
2021-04-04 22:15:09 +0900

    makearray2d

--- 5week/makeArray1D.cpp
+++ 5week/makeArray1D.cpp
@@ -2,26 +2,42 @@
 #include <cstdlib>
 using namespace std;
 
+//1차원 배열 만들어서 반환하기 
+
 int *makeArray1D(int *sz);
 void destroyArray1D(int *arr, int *sz);
 
+//우선 1차원이므로 argc = 2
 int main(int argc, char *argv[]){
     if(argc <2){
         cout << "usage : ./str  1d 2d 3d... nd\n";
         return -1;
     }
 
+    //i, dim = 1;
     int i, dim = argc-1;
+
+
     int *size = new int [dim];
 
     for(i = 1; i<argc; i++) size[i-1] = atoi(argv[i]);
 
-    int *arrld = NULL;
+    int *arr1d = NULL;
 
-    arrld = makeArray1D(size);
-    for (int i=0; i<size[0]; i++) arrld[i]=i;
-    for (int i=0; i<size[0]; i++) cout << arrld[i] << " ";
+    arr1d = makeArray1D(size);
+    for (int i=0; i<size[0]; i++) arr1d[i]=i;
+    for (int i=0; i<size[0]; i++) cout << arr1d[i] << " ";
     cout << endl;
-    destroyArray1D(arrld, size);
+    destroyArray1D(arr1d, size);
     return 0;
+}
+
+int *makeArray1D (int *sz){
+    int n = sz[0];
+    int *arr = new int[n];
+    return arr;
+}
+
+void destroyArray1D(int *arr, int *sz){
+    delete[] arr;
 }
