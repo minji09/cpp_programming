ae9b1965d8ccdf6d7311b0bdad4fe0918d251448
minji09 <eming98@naver.com>
2021-04-07 21:33:32 +0900

    polygonArea

--- homework/5week_homework/polygonArea.cpp
+++ homework/5week_homework/polygonArea.cpp
@@ -1,15 +1,34 @@
 #include <iostream>
+#include <string>
 #include <cmath>
+#include <typeinfo>
 using namespace std;
 
 int main(){
-    int n, a;
+
+    int n;
+    cin >> n;
+    int *x = new int [n];
+    int *y = new int [n];
+    for (int i=0; i<n; i++) cin >> x[i] >> y[i];
     
-    cin >> n ;
-    
-    for (int i =1; i<=2*n; i++){
-        cin >> a;
+    int signedArea =0;
+    int area;
+
+    for (int i = 0; i<n; i++) {
+        if(i+1 == n) signedArea += (x[i]+x[0])*(y[0]-y[i]);
+        else signedArea += (x[i]+x[i+1])*(y[i+1]-y[i]);
     }
-    cout << n << a;
+    
+    area = abs(signedArea);
+    
+    int check;
+    if (signedArea <0) check=-1;
+    else check = 1;
+
+    cout << area <<" "<< check << endl;
+
+
+
     return 0;
 }
