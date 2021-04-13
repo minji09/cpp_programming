cdb5bae39bfa6a5274aea8b95ff532c48cba7b2b
minji09 <eming98@naver.com>
2021-04-13 23:27:57 +0900

    hamming_number

--- test/test2.cpp
+++ test/test2.cpp
@@ -1,7 +1,33 @@
-vector<int> diff;
-	diff.resize(v1.size()+v2.size());
-	auto iter = set_difference(v1.begin(),v1.end(), v2.begin(), v2.end(), diff.begin());
-	diff.erase(iter,diff.end());
-	cout << diff.size() << " ";
-	for_each(diff.begin(), diff.end(), [](int i){cout << i << " ";});
-	cout << endl;
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+
+
+int main(){
+
+    int n;
+    cin >> n;
+    vector<unsigned int> v = {1,2,3,5};
+	vector<int> s = {2,3,5};
+
+
+	unsigned int k=0;
+	while(v.size()<=5000){
+		int m = v.size();
+		for (int i = 0; i<m; i++){
+			for (int j = 0; j<3; j++){
+				k = v[i]*s[j];
+				v.push_back(k);
+			}
+		}
+		sort(v.begin(),v.end());
+		v.erase(unique(v.begin(),v.end()),v.end());
+
+	}
+	cout <<v[n-1];
+	
+
+    return 0;
+}
